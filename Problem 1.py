668d3fa543b65b91108bd5de5f2f0db3da34f274
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-12-24 00:37:26 +0530

    By Google - Adding two numbers from a list

---
+++ Problem 1.py
@@ -0,0 +1,20 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+"""
+def solution(nums, k):
+    for i in nums:
+        remaining = abs(i-k)
+        if remaining in nums:
+            return True
+    return False
+
+#Driver code
+print(solution([10, 15, 3, 7],17))
